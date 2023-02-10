18d052d5baf33a6e7d4a36059c5e0fbfa2166c91
opilane <opilane@HP-CZC2349HT0>
2023-02-10 13:59:35 +0200

    Add project files.

---
+++ Funktsioon/Funktsioon.py
@@ -0,0 +1,42 @@
+﻿
+from math import*
+from random import*
+from module1 import *
+
+#(3)
+
+
+
+
+
+
+
+
+print()
+print()
+# (2)
+spin2=is_year_leap(float(input("a1: ")))
+print(spin2)
+    
+
+
+
+print()
+print()
+# (1)
+spin=arithmetic(float(input("a:")),input("faraon:"),float(input("b:")))
+print(spin)
+
+
+
+#a=int(input("Введите первое число: "))
+#b=int(input("Введите второе число: "))
+#print("Результат: ", a , b)
+
+
+
+
+
+
+
+
---
+++ Funktsioon/module1.py
@@ -0,0 +1,63 @@
+﻿def square(tehe:float)->any:
+    """
+    Määrme square...
+    :parem tehe a2: Järjend square numbridest
+    :rtype: str
+    """
+    tehe=a*4
+
+
+
+
+
+
+
+
+
+
+#2
+
+def  is_year_leap(a1:int)->bool:
+    """
+    Määrme is_year_leap...
+    :parem int a1: Järjend is_year_leap numbridest
+    :rtype: str
+    """
+    if a1%4==0:
+         t=True 
+
+    else:
+        t= False
+
+    return t
+        
+
+       
+
+#1
+
+def arithmetic(a:float,faraon:str, b:float)->any:
+    """
+    Määrme arithmetic....
+    :parem str faraon,float a, float b: Järjend arithmetic numbridest
+    :rtype: str
+    """
+    if faraon=='+':
+        vastus= a+b
+    elif faraon=="-":
+        vastus= a-b
+    elif faraon=="*":
+        vastus= a*b
+    elif faraon=="/":
+        if b==0:
+            vastus="DIV0"
+        else:
+            vastus=a/b
+
+    else:
+         vastus="Tundmatu tehe"
+    return vastus
+
+
+
+
